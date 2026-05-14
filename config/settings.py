461c099e820f4b54cc315eca1c6a3c168b309c0c
BONAFO <bgbcnnvb@gmail.com>
2026-05-14 17:19:16 -0300

    Terminado navbar

--- config/settings.py
+++ config/settings.py
@@ -126,8 +126,11 @@ USE_I18N = True
 USE_TZ = True
 
 
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
